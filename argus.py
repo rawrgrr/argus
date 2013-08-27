ab067b28ea90d41686aba188111d235654904969
Roger Tu <rtu@ocf.berkeley.edu>
2013-08-27 15:44:35 -0700

    Initial commit of argus.py

---
+++ argus.py
@@ -0,0 +1,25 @@
+import os
+import cherrypy
+
+class Argus:
+    @cherrypy.expose
+    def index(self, branch):
+        return branch
+
+class Root:
+    pass
+
+root = Root()
+
+root.argus = Argus()
+
+conf = {
+    '/': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.abspath(os.path.dirname(__file__)),
+        'tools.staticdir.index': 'index.html',
+        },
+    }
+
+cherrypy.quickstart(root, config=conf)
+
